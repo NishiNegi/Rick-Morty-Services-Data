38de4611666efb38d514163cbfc092f4c418fd0d
Juan Camilo <orjuela9@gmail.com>
2022-10-26 16:27:38 -0500

    Create base files

---
+++ index.js
@@ -0,0 +1,4 @@
+const start = require("./services.js");
+
+const search = process.argv[2];
+const id = process.argv[3];
---
+++ services.js
@@ -0,0 +1,3 @@
+const axios = require("axios");
+
+const rickAndMortyApi = "https://rickandmortyapi.com/api";
