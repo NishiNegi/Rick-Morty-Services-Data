e05fe72804c0f9cbe103caacb6f4e02fe29099d1
Juan Camilo <orjuela9@gmail.com>
2022-10-26 16:34:59 -0500

    Create conditionals for possible process.argv inputs

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const start = require("./services.js");
 
 const search = process.argv[2];
 const id = process.argv[3];
+
+if (search === "personajes" && id === undefined) {
+  //start getCharacters function
+} else if (search === "episodios" && id === undefined) {
+  //start getEpisodes function
+} else if (search === "personajes" && id !== undefined) {
+  //start getCharactersById function
+} else if (search === "episodios" && id !== undefined) {
+  //start getEpisodesById function
+}
